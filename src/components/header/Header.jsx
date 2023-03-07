74c7fba99d5c201974d5cadcb2c28375a0433276
hemant1101 <hemantmandal1101@gmail.com>
2023-03-07 23:22:34 +0530

    logo change

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,7 +5,7 @@ import { VscChromeClose } from 'react-icons/vsc'
 import { useNavigate, useLocation } from 'react-router-dom'
 import './style.scss'
 import ContentWrapper from '../contentWrapper/ContentWrapper'
-import logo from '../../assets/cinemix-logo.svg'
+import logo from '../../assets/movix-logo.png'
 
 const Header = () => {
   const [show, setShow] = useState("top")
